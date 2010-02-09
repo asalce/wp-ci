9f7e0bb132c0250086ef1fd4e335fd1062433745
Alberto Salce <alberto.salce@gmail.com>
2010-02-09 12:52:30 -0500

    removing @function annotation.

--- examples/hello-sparky/application/controllers/demo.php
+++ examples/hello-sparky/application/controllers/demo.php
@@ -1,13 +1,11 @@
 <?php
 /** 
 * @user_can(manage_options) 
-* @function(__construct)
 */
 class Demo extends Controller {
 
   /** 
   * @menu(page_title=Hello Sparky&menu_title=Hello Sparky) 
-  * @function(index)
   */
   function index() {
     $this->load->view('demo');
